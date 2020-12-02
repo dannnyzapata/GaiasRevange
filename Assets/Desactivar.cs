f0f5836482c36a8467da8d85d275a0641186dd6f
danny <a299474@uach.mx>
2020-12-01 21:17:30 -0700

    Subiendo Repo limpio
    Repo limpio para entrega

---
+++ Assets/Desactivar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Desactivar : MonoBehaviour
+{
+    public Button Boton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ActivarBoton());
+    }
+
+    public IEnumerator ActivarBoton()
+    {
+
+        Boton = gameObject.GetComponent<Button>();
+
+        Boton.interactable = false;
+        yield return new WaitForSeconds(6f);
+        Boton.interactable = true; 
+
+
+
+    }
+
+}
